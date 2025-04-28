c1f37db4ac2534469256a2eec27862810c7a206a
Link-Yundi <7795351+link_yundi@user.noreply.gitee.com>
2025-04-28 11:30:29 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------
+Created on 2024/6/30 02:37
+@author: ZhangYundi
+@email: yundi.xxii@outlook.com
+---------------------------------------------
+"""
+
+from .ylog import info, warning, debug, error, add
+
+__all__ = ["info", "warning", "debug", "error", "add"]
